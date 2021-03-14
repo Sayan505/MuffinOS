d6544108d16f816ddafdf41d56bca3a5482c7266
Sayan505 <bsayan8086@gmail.com>
2021-03-15 01:27:59 +0530

    trim exterms

--- kernel/extern/video_vars.h
+++ kernel/extern/video_vars.h
@@ -13,4 +13,3 @@
 extern stiletto_video_t video;         // kernel's instance of stiletto_video_t
 
 extern uint32_t v_color;               // current draw color; 32bpp
-extern pixel_t  rgb;                   // EFI_PIXEL_BITMASK; 32bpp
--- kernel/sys/video/video_vars.h
+++ kernel/sys/video/video_vars.h
@@ -6,4 +6,3 @@
 stiletto_video_t video;         // kernel's instance of stiletto_video_t
 
 uint32_t v_color;               // current draw color; 32bpp
-pixel_t  rgb;                   // EFI_PIXEL_BITMASK; 32bpp
