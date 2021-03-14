b27e97be6f31eebe009c1eeb89c0bdd7588a7b69
Sayan505 <bsayan8086@gmail.com>
2021-03-15 01:31:24 +0530

    trim data types

--- kernel/extern/video_vars.h
+++ kernel/extern/video_vars.h
@@ -6,10 +6,6 @@
 
 #include <sys/bootprotocol/stiletto_video.h>
 
-// Pixel Format
-#include <sys/video/pixel.h>
-
-
 extern stiletto_video_t video;         // kernel's instance of stiletto_video_t
 
 extern uint32_t v_color;               // current draw color; 32bpp
--- kernel/sys/video/video.h
+++ kernel/sys/video/video.h
@@ -6,9 +6,6 @@
 
 #include <sys/bootprotocol/stiletto_video.h>
 
-// Pixel Format
-#include <sys/video/pixel.h>
-
 // global vars
 #include <sys/video/video_vars.h>
 
