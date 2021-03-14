61f73c6e3a6d7197600841447eded6422b129f1f
Sayan505 <bsayan8086@gmail.com>
2021-03-15 01:05:56 +0530

    trim

--- kernel/sys/devices/io_ports.h
+++ kernel/sys/devices/io_ports.h
@@ -30,7 +30,7 @@ static inline uint8_t inb(uint16_t port) {
     __asm__ volatile ("inb %1, %0"
                       : "=a"(datb)
                       : "Nd"(port)
-                      );
+                     );
     
     return datb;
 }
--- kernel/sys/video/video.h
+++ kernel/sys/video/video.h
@@ -12,9 +12,10 @@
 // global vars
 #include <sys/video/video_vars.h>
 
-// global subroutines
+// global subroutines (incl. after vars)
 #include <sys/video/video_subroutines.h>
 
 
 // TODO: Implement overloads for typedef struct { ... } pixel_t, color_t;
 // TODO: Implement a global set_color subroutine.
+// TODO: Font renderer
